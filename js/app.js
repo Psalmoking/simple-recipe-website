79543c3c38ffe342bdf0e50e7beb61de9dc23e74
Samuel Adeleke <68246406+Psalmoking@users.noreply.github.com>
2022-08-29 16:59:07 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,27 @@
+// const navLinks = document.querySelector(".nav-links")
+// const navBtn = document.querySelector(".nav-btn")
+
+// function toggleNavbar () {
+//   navLinks.classList.toggle("hidden")
+// }
+
+// navBtn.addEventListener("click", toggleNavbar)
+
+const getElement = (selector) => {
+  const element = document.querySelector(selector)
+  if (element) return element 
+  throw Error(
+    `Please double check your class names, there is no ${selector} class`
+  )
+}
+
+const links = getElement(".nav-links")
+const navBtnDOM = getElement('.nav-btn')
+
+navBtnDOM.addEventListener("click", () => {
+  links.classList.toggle("show-links")
+})
+
+const date = getElement('#date')
+const currentYear = new Date().getFullYear();
+date.textContent = currentYear;
